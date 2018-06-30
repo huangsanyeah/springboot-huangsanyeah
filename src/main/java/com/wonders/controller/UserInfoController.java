c89ae59a1811eb35e1537aa5058a18ec95f93510
huangweiyue <huangweiyue@quyiyuan.com>
2018-06-30 22:36:14 +0800

    初始化代码块

--- src/main/java/com/wonders/controller/UserInfoController.java
+++ src/main/java/com/wonders/controller/UserInfoController.java
@@ -4,6 +4,7 @@ import com.wonders.entity.UserInfo;
 import com.wonders.service.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,5 +20,14 @@ public class UserInfoController {
     @RequestMapping("/getUser")
     public UserInfo getUser(Long uid){
         return userInfoService.getUser(uid);
+    }
+
+    /**
+     * *想要看一个注解，直接点进去 比如 RequestParam 就可以看到有什么参数可以设置了 比如name defaultValue
+     **/
+
+    @RequestMapping("/zhujie")
+    public UserInfo getr(@RequestParam() String name) {
+        return null;
     }
 }
