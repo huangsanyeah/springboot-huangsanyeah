537079f2ab08d536ccb566956cdc069b79b00da9
huangweiyue <huangweiyue@quyiyuan.com>
2018-08-26 19:39:03 +0800

    @DateTimeFormat注解使用

--- src/main/java/com/wonders/controller/UserInfoController.java
+++ src/main/java/com/wonders/controller/UserInfoController.java
@@ -2,17 +2,23 @@ package com.wonders.controller;
 
 import com.wonders.entity.UserInfo;
 import com.wonders.service.UserInfoService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 /**
  * @author huangwieyue
  * @date 2018-02-13 11:48
  */
 @RestController
-@RequestMapping("springboot")
+@RequestMapping("/springboot")
+@Slf4j
 //http://localhost:8075/springboot/getUser?uid=1
 public class UserInfoController {
     @Autowired
@@ -29,5 +35,18 @@ public class UserInfoController {
     @RequestMapping("/zhujie")
     public UserInfo getr(@RequestParam() String name) {
         return null;
+    }
+
+    /**
+     * dateTimeFormat注解测试
+     * http://localhost:8075/springboot/dateFormat?day=2018/01/02 10:32:04
+     *
+     * @param day
+     * @return
+     */
+    @GetMapping("/dateFormat")
+    public String testDateTimeFormatAnnotation(@DateTimeFormat(iso = DateTimeFormat.ISO.DATE, pattern = "yyyy/mm/dd hh:mm:ss") Date day) {
+        log.info("入参day--->" + day);
+        return day.toString();
     }
 }
---
+++ src/main/java/com/wonders/reflect/ReflectDemo1.java
@@ -0,0 +1,18 @@
+package com.wonders.reflect;
+
+/**
+ * @author huangweiyue
+ * @version v1.0
+ * @task
+ * @description 反射
+ * @date Created in 2018-08-26
+ * @modifiedBy
+ */
+public class ReflectDemo1 {
+    public static void main(String[] args) {
+        Class c1 = int.class;
+        System.out.println(c1);
+        Class c2 = Integer.class;
+        System.out.println(c2);
+    }
+}
