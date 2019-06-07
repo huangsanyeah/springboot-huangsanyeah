0b0485209df6fec40838a355539212384ce27306
huangweiyue <huangweiyue@quyiyuan.com>
2019-06-07 23:57:51 +0800

    java值传递 int Integer测试

--- src/main/java/com/wonders/test/ValueDeliverTest.java
+++ src/main/java/com/wonders/test/ValueDeliverTest.java
@@ -13,8 +13,22 @@ import lombok.Data;
 public class ValueDeliverTest {
     public static void main(String[] args) {
         Circle circle = new Circle(2);
+        System.out.println("引用类型before:"+circle);
         changeCircleRValue(circle);
-        System.out.println(circle);
+        System.out.println("引用类型after:"+circle);
+
+
+        int a=1;
+        System.out.println("int基本类型before:"+a);
+        changeIntValue(a);
+        System.out.println("int基本类型after:"+a);
+
+        Integer b=3;
+        System.out.println("Integer基本类型before:"+b);
+        changeIntegerValue(a);
+        System.out.println("Integer基本类型after:"+b);
+
+
     }
 
     private static void changeCircleRValue(Circle circle){
@@ -22,6 +36,22 @@ public class ValueDeliverTest {
         circle=new Circle(3);
         //code2 输出3
         //circle.setR(3);
+    }
+
+    /**
+     * int基本类型
+     * @param value
+     */
+    private static void changeIntValue(int value){
+        value=2;
+    }
+
+    /**
+     * Integer包装类
+     * @param value
+     */
+    private static void changeIntegerValue(Integer value){
+        value=4;
     }
 }
 
