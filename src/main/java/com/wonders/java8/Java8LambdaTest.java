af70ca00c7d8f6fa054edffa5744675c19a12788
huangweiyue <huangweiyue@quyiyuan.com>
2019-06-22 22:14:35 +0800

    java8 lambda表达式

---
+++ src/main/java/com/wonders/java8/Java8LambdaTest.java
@@ -0,0 +1,54 @@
+package com.wonders.java8;
+
+/**
+ * @description 菜鸟教程 java8 lambda
+ * @date Created in 2019-06-22
+ */
+public class Java8LambdaTest {
+    public static void main(String args[]){
+        Java8LambdaTest tester = new Java8LambdaTest();
+
+        // 类型声明
+        MathOperation addition = (int a, int b) -> a + b;
+
+        // 不用类型声明
+        MathOperation subtraction = (a, b) -> a - b;
+
+        // 大括号中的返回语句
+        MathOperation multiplication = (int a, int b) -> { return a * b; };
+
+        // 没有大括号及返回语句
+        MathOperation division = (int a, int b) -> a / b;
+
+        /**
+         * Lambda 允许把函数作为一个方法的参数（函数作为参数传递进方法中）
+         */
+        System.out.println("10 + 5 = " + tester.operate(10, 5, addition));
+        System.out.println("10 - 5 = " + tester.operate(10, 5, subtraction));
+        System.out.println("10 x 5 = " + tester.operate(10, 5, multiplication));
+        System.out.println("10 / 5 = " + tester.operate(10, 5, division));
+
+        // 不用括号
+        GreetingService greetService1 = message ->
+                System.out.println("Hello " + message);
+
+        // 用括号
+        GreetingService greetService2 = (message) ->
+                System.out.println("Hello " + message);
+
+        greetService1.sayMessage("Runoob");
+        greetService2.sayMessage("Google");
+    }
+
+    interface MathOperation {
+        int operation(int a, int b);
+    }
+
+    interface GreetingService {
+        void sayMessage(String message);
+    }
+
+    private int operate(int a, int b, MathOperation mathOperation){
+        return mathOperation.operation(a, b);
+    }
+}
