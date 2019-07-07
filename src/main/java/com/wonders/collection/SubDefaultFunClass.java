1c97299be82ff6170ed0e3caaebd7f480ff00d9d
huangweiyue <huangweiyue@quyiyuan.com>
2019-07-07 16:22:09 +0800

    默认方法

--- src/main/java/com/wonders/collection/SubDefaultFunClass.java
+++ src/main/java/com/wonders/collection/SubDefaultFunClass.java
@@ -10,7 +10,7 @@ package com.wonders.collection;
  */
 public class SubDefaultFunClass implements DefaultFunInterface {
     public static void main(String[] args) {
-    //实例化一个子类对象，改子类对象可以直接调用父接口中的默认方法 count
+        //实例化一个子类对象，该子类对象可以直接调用父接口中的默认方法 count
         SubDefaultFunClass sub = new SubDefaultFunClass();
         int value = sub.count();
         System.out.println("result：" + value);
